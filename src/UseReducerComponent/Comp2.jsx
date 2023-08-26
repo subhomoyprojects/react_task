d090d7c2d9cdc3509f5126dff48185d263acf7d8
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-08-26 23:42:34 +0530

    switch on off reducer

--- src/UseReducerComponent/Comp2.jsx
+++ src/UseReducerComponent/Comp2.jsx
@@ -1,5 +1,32 @@
-import React from "react";
-
+import React, { useReducer } from "react";
+import BulbOn from "../images/pic_bulbon.gif";
+import BulbOff from "../images/pic_bulboff.gif";
+const render = (state, action) => {
+  switch (action.type) {
+    case "LIGHT_STATE": {
+      return (state = !state);
+    }
+    default: {
+      return state;
+    }
+  }
+};
 export default function Comp2() {
-  return <h1>Comp2</h1>;
+  const [state, dispatch] = useReducer(render, false);
+
+  return (
+    <div>
+      <h1>Comp2</h1>
+      <img src={state ? BulbOn : BulbOff} alt="" />
+      <button
+        type="button"
+        className="btn btn-info"
+        onClick={() => {
+          dispatch({ type: "LIGHT_STATE" });
+        }}
+      >
+        {state ? "Switch On" : "Switch Off"}
+      </button>
+    </div>
+  );
 }
